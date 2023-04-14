92b17ededef61fcb23eed219cda0d6d497216739
mahdi-gasemfar <96542298+mahdi-gasemfar@users.noreply.github.com>
2023-04-14 16:32:46 +0330

    add slug

--- app/models.py
+++ app/models.py
@@ -1,7 +1,6 @@
 from django.db import models
+from django.utils.text import slugify
 
-
-# Create your models here.
 
 class JobPost(models.Model):
     objects = None
@@ -9,7 +8,12 @@ class JobPost(models.Model):
     description = models.CharField(max_length=200)
     data = models.DateTimeField(auto_now_add=True)
     salary = models.IntegerField()
+    slug = models.SlugField(null=True, max_length=40, unique=True)
+
+    def save(self, *args, **kwargs):
+        if not self.id:
+            self.slug = slugify(self.title)
+        return super(JobPost, self).save(*args, **kwargs)
 
     def __str__(self):
         return self.title
-
