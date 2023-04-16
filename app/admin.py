83a47cb96604889d55d5d07958a3f4e0ba377a87
mahdi-gasemfar <96542298+mahdi-gasemfar@users.noreply.github.com>
2023-04-16 15:11:07 +0330

    add list filter to admin panel

--- app/admin.py
+++ app/admin.py
@@ -4,6 +4,7 @@ from app.models import JobPost
 
 class JobAdmin(admin.ModelAdmin):
     list_display = ("__str__", "title", "date", "salary",)
+    list_filter = ("date", "salary", "expiry")
 
 
 admin.site.register(JobPost, JobAdmin)
