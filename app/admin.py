031f64c5314dc9d8d5b3284b068fc55d6cbfb7aa
mahdi-gasemfar <96542298+mahdi-gasemfar@users.noreply.github.com>
2023-04-17 18:05:30 +0330

    add search field to admin panel

--- app/admin.py
+++ app/admin.py
@@ -5,6 +5,8 @@ from app.models import JobPost
 class JobAdmin(admin.ModelAdmin):
     list_display = ("__str__", "title", "date", "salary",)
     list_filter = ("date", "salary", "expiry")
+    search_fields = ["title", "description"]
+    search_help_text = "You can Search Any thing you want"
 
 
 admin.site.register(JobPost, JobAdmin)
