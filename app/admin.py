8624c6c9538b4234e920b03997eaf0fe1057d767
mahdi-gasemfar <96542298+mahdi-gasemfar@users.noreply.github.com>
2023-04-17 18:52:18 +0330

    update admin panel

--- app/admin.py
+++ app/admin.py
@@ -7,6 +7,17 @@ class JobAdmin(admin.ModelAdmin):
     list_filter = ("date", "salary", "expiry")
     search_fields = ["title", "description"]
     search_help_text = "You can Search Any thing you want"
+    #fields = (("title", "description"), "expiry")
+    #exclude = ("title", )
+    fieldsets = (
+        ("Basic information", {
+        "fields": ("title", "description")
+         }),
+        ("More information", {
+            "classes":("collapse", ),
+            "fields": (("salary", "expiry"), "slug")
+        }),
+    )
 
 
 admin.site.register(JobPost, JobAdmin)
