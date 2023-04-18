7832bb53424ffdb2412c358901d04c4a44164a69
mahdi-gasemfar <96542298+mahdi-gasemfar@users.noreply.github.com>
2023-04-18 21:58:43 +0330

    add location model in database

--- app/models.py
+++ app/models.py
@@ -2,6 +2,14 @@ from django.db import models
 from django.utils.text import slugify
 
 
+class Location(models.Model):
+    street = models.CharField(max_length=200)
+    city = models.CharField(max_length=200)
+    state = models.CharField(max_length=200)
+    country = models.CharField(max_length=200)
+    zip = models.CharField(max_length=200)
+
+
 class JobPost(models.Model):
     objects = None
     title = models.CharField(max_length=200)
@@ -10,6 +18,7 @@ class JobPost(models.Model):
     expiry = models.DateField(null=True)
     salary = models.IntegerField()
     slug = models.SlugField(null=True, max_length=40, unique=True)
+    location = models.OneToOneField(Location, null=True, on_delete=models.CASCADE)
 
     def save(self, *args, **kwargs):
         if not self.id:
