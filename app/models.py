58f1be1f9f1a2c9515df5c4b06940a50ee8d16ee
mahdi-gasemfar <96542298+mahdi-gasemfar@users.noreply.github.com>
2023-04-19 13:03:14 +0330

    update models.py - add author in our app

--- app/models.py
+++ app/models.py
@@ -2,6 +2,12 @@ from django.db import models
 from django.utils.text import slugify
 
 
+class Author(models.Model):
+    name = models.CharField(max_length=200)
+    company = models.CharField(max_length=200)
+    designation = models.CharField(max_length=200)
+
+
 class Location(models.Model):
     street = models.CharField(max_length=200)
     city = models.CharField(max_length=200)
@@ -18,7 +24,10 @@ class JobPost(models.Model):
     expiry = models.DateField(null=True)
     salary = models.IntegerField()
     slug = models.SlugField(null=True, max_length=40, unique=True)
-    location = models.OneToOneField(Location, null=True, on_delete=models.CASCADE)
+    location = models.OneToOneField(Location, null=True,
+                                    on_delete=models.CASCADE)
+    author = models.ForeignKey(Author, on_delete=models.CASCADE,
+                               null=True)
 
     def save(self, *args, **kwargs):
         if not self.id:
