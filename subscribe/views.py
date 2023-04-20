9af0c5648369c7f4a6b1d801a1950181a649fde2
mahdi-gasemfar <96542298+mahdi-gasemfar@users.noreply.github.com>
2023-04-20 13:34:09 +0330

    update forms.py

--- subscribe/views.py
+++ subscribe/views.py
@@ -1,20 +1,14 @@
 from django.shortcuts import render
 from subscribe.models import Subscribe
-from subscribe.forms import  SubscribeForm
+from subscribe.forms import SubscribeForm
+
 
 def subscribe(request):
     subscribe_form = SubscribeForm()
     email_error_empty = ""
     if request.POST:
-        first_name = request.POST["firstname"]
-        last_name = request.POST["lastname"]
-        email = request.POST["email"]
-        print("POST REQUEST : ", email)
-        if email == "":
-            email_error_empty = "No Email entered"
-
-        sb = Subscribe(first_name=first_name, last_name=last_name,
-                       email=email)
-        sb.save()
-    context = {"form":subscribe_form, "email_error_empty": email_error_empty}
+        subscribe_form = SubscribeForm(request.POST)
+        if subscribe_form.is_valid():
+            print("valid form")
+    context = {"form": subscribe_form, "email_error_empty": email_error_empty}
     return render(request, "subscribe/subscribe.html", context)
