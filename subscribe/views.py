ebab01fa4f72db024c218918a27413999a911b3f
mahdi-gasemfar <96542298+mahdi-gasemfar@users.noreply.github.com>
2023-04-20 11:20:49 +0330

    update subscribe

--- subscribe/views.py
+++ subscribe/views.py
@@ -2,5 +2,11 @@ from django.shortcuts import render
 
 
 def subscribe(request):
-    context = {}
+    email_error_empty = ""
+    if request.POST:
+        email = request.POST["email"]
+        print("POST REQUEST : ", email)
+        if email == "":
+            email_error_empty = "No Email entered"
+    context = {"email_error_empty": email_error_empty}
     return render(request, "subscribe/subscribe.html", context)
