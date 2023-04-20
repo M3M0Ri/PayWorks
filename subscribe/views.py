c2c7f3291336727216a61bf70d04bc8cd44794d9
mahdi-gasemfar <96542298+mahdi-gasemfar@users.noreply.github.com>
2023-04-20 11:37:42 +0330

    connect form to database

--- subscribe/views.py
+++ subscribe/views.py
@@ -1,12 +1,19 @@
 from django.shortcuts import render
+from subscribe.models import Subscribe
 
 
 def subscribe(request):
     email_error_empty = ""
     if request.POST:
+        first_name = request.POST["firstname"]
+        last_name = request.POST["lastname"]
         email = request.POST["email"]
         print("POST REQUEST : ", email)
         if email == "":
             email_error_empty = "No Email entered"
+
+        sb = Subscribe(first_name=first_name, last_name=last_name,
+                       email=email)
+        sb.save()
     context = {"email_error_empty": email_error_empty}
     return render(request, "subscribe/subscribe.html", context)
