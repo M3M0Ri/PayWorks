88da30e136c9717ed878d43ee41c33b7dd91f48c
mahdi-gasemfar <96542298+mahdi-gasemfar@users.noreply.github.com>
2023-04-22 14:45:26 +0330

    update forms.py

--- subscribe/forms.py
+++ subscribe/forms.py
@@ -2,6 +2,12 @@ from django import forms
 
 
 class SubscribeForm(forms.Form):
-    first_name = forms.CharField(max_length=100)
+    first_name = forms.CharField(max_length=100, required=False)
     last_name = forms.CharField(max_length=100)
     email = forms.EmailField(max_length=100)
+
+    def clean_first_name(self):
+        data = self.cleaned_data['first_name']
+        if "," in data:
+            raise forms.ValidationError("Invalid First Name")
+        return data
