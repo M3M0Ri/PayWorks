cb98c10ae99612aa505b2f6a39981d266252f672
mahdi-gasemfar <96542298+mahdi-gasemfar@users.noreply.github.com>
2023-04-22 19:07:31 +0330

    update models.py

--- subscribe/models.py
+++ subscribe/models.py
@@ -1,10 +1,16 @@
 from django.db import models
 
+NEWLETTER_OPTION = [
+    ("W", "weekly"),
+    ("M", "monthly")
+]
+
 
 class Subscribe(models.Model):
     first_name = models.CharField(max_length=100)
     last_name = models.CharField(max_length=100)
     email = models.CharField(max_length=100)
+    option = models.CharField(max_length=2, choices=NEWLETTER_OPTION)
 
     def __str__(self):
         return f"{self.first_name} ->  {self.email}"
