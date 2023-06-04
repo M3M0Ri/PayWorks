708629f142ead089dbaa647c12bd727709042f3a
mahdi-gasemfar <96542298+mahdi-gasemfar@users.noreply.github.com>
2023-06-04 08:08:37 +0330

    save image

--- uploadapp/views.py
+++ uploadapp/views.py
@@ -5,5 +5,10 @@ from uploadapp.forms import UploadFrom
 # Create your views here.
 
 def upload_image(request):
-    form = UploadFrom()
+    if request.method == "POST":
+        form = UploadFrom(request.POST, request.FILES)
+        if form.is_valid():
+            form.save()
+    else:
+        form = UploadFrom()
     return render(request, "uploadapp/add_image.html", {"form":form})
