c44ee399f41fc280efc36807382867e8bda170aa
mahdi-gasemfar <96542298+mahdi-gasemfar@users.noreply.github.com>
2023-06-04 08:49:36 +0330

    save and display image

--- PayWorks/settings.py
+++ PayWorks/settings.py
@@ -123,6 +123,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 MEDIA_ROOT = BASE_DIR / "uploads/"
+MEDIA_URL = "/media/"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
--- PayWorks/urls.py
+++ PayWorks/urls.py
@@ -16,13 +16,14 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from django.http import HttpResponse
+from django.conf.urls.static import static
+from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("", include("app.urls")),
     path("", include("subscribe.urls")),
     path("uploads/", include("uploadapp.urls")),
-
-]
+   ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 
